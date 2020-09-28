c047c834bff0cd9bcae6471fe01c8577a929fe07
Alexandra Cherdantseva <neluhus.vagus@gmail.com>
2020-09-28 18:10:13 +0300

    fix close namespace order

--- targets/qtscript/templates/register.c
+++ targets/qtscript/templates/register.c
@@ -1,4 +1,4 @@
-#for namespace_name in $current_class.namespace_list
+#for namespace_name in reversed($current_class.namespace_list)
 } // end of ${namespace_name}
 
 #end for
--- targets/qtscript/templates/register.h
+++ targets/qtscript/templates/register.h
@@ -1,5 +1,5 @@
 };
-#for namespace_name in $current_class.namespace_list
+#for namespace_name in reversed($current_class.namespace_list)
 
 } // end of ${namespace_name}
 #end for
