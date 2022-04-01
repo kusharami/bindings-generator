1ba7f6c9013e00639a67f0cbb9149cf911cf2919
Alexandra Cherdantseva <neluhus.vagus@gmail.com>
2022-04-01 10:19:12 +0300

    qtscript: don't crash if engine is destroyed when script is called from wrapped std::function

--- targets/qtscript/templates/lambda.c
+++ targets/qtscript/templates/lambda.c
@@ -1,8 +1,22 @@
 #set ReturnType = $ret_type.namespaced_name
-!${in_value}.isFunction() ? ${namespaced_name}() : [=](${lambda_parameters}) mutable -> $ReturnType
+!${in_value}.isFunction() ? ${namespaced_name}() : [=](${lambda_parameters}) mutable#if $ReturnType != "void" # -> $ReturnType#end if #
 {
+#set ret_type_str = $ret_type.to_string($generator)
 #set arg_idx = 0
 #set arg_count = len($param_types)
+	if (!${in_value}.engine()) {
+#if $ret_type_str == "void"
+		return;
+#elif $ret_type_str == "bool"
+		return false;
+#elif $ret_type_str.endswith('*')
+		return nullptr;
+#elif $ret_type.is_enum or $ret_type.is_numeric
+		return static_cast<${ret_type_str}>(0);
+#else
+		return ${ret_type_str}();
+#end if
+	}
 	QScriptValueList arguments;
 #while $arg_idx < $arg_count
 	#set arg = $param_types[$arg_idx]
@@ -18,7 +32,6 @@
 #if $ReturnType == "void"
 	${from_qtscript};
 #else
-	#set ret_type_str = $ret_type.to_string($generator)
 	#if $ret_type_str != 'QScriptValue'
 		#set $from_qtscript = "qscriptvalue_cast<{}>({})".format($ret_type_str, $from_qtscript)
 	#end if
